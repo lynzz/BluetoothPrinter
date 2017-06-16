72857f71c14fad02f0cadc7add91515d5f52d44e
lynzz <lynzz168@gmail.com>
2017-06-16 23:54:31 +0800

    add corebluetooth

--- src/ios/BluetoothPrinter.h
+++ src/ios/BluetoothPrinter.h
@@ -1,7 +1,20 @@
-#import <Cordova/CDVPlugin.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <CoreBluetooth/CBService.h>
+#import <Cordova/CDV.h>
 
-@interface BluetoothPrinter : CDVPlugin
+@interface BluetoothPrinter : CDVPlugin <CBCentralManagerDelegate, CBPeripheralDelegate, CBPeripheralManagerDelegate> {
+    NSString* _connectCallbackId;
+    NSString* _listCallbackId;
+    NSData* _printData;
+}
 
-- (void)getAppName:(CDVInvokedUrlCommand*)command;
+@property (strong, nonatomic) CBCentralManager *_centralManager;
+@property (strong, nonatomic) NSMutableArray *_peripherals;
+@property (strong, nonatomic) CBPeripheral *_activePeripheral;
+
+- (void)open:(CDVInvokedUrlCommand *)command;
+- (void)close:(CDVInvokedUrlCommand *)command;
+- (void)print:(CDVInvokedUrlCommand *)command;
+- (void)list:(CDVInvokedUrlCommand *)command;
 
 @end
--- www/BluetoothPrinter.js
+++ www/BluetoothPrinter.js
@@ -1,5 +1,21 @@
 var exec = require('cordova/exec');
 
-exports.getAppName = function(success, error) {
-    exec(success, error, "BluetoothPrinter", "getAppName");
+var printer = {
+   list: function(fnSuccess, fnError){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "list", []);
+   },
+   open: function(fnSuccess, fnError, name){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "open", [name]);
+   },
+   close: function(fnSuccess, fnError){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "close", []);
+   },
+   print: function(fnSuccess, fnError, str){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
+   },
+   connect: function (success, failure, name) {
+      exec(success, failure, "BluetoothPrinter", "connect", [name]);
+   }
 };
+
+module.exports = printer;
